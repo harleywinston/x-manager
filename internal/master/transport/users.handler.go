8753785edaefbae72b4afa2a54825daacaa1f966
harley winston <harleywinston1993@proton.me>
2023-05-02 20:27:02 +0330

    [ ADD ] user handlers

--- internal/master/transport/users.handler.go
+++ internal/master/transport/users.handler.go
@@ -12,19 +12,69 @@ type UsersHandler struct {
 }
 
 func (h *UsersHandler) GetUserHandler(ctx *gin.Context) {
-	user := models.UsersModel{}
-	user.Email = ""
-	_, _ = h.userSerivce.GetUserService(user)
+	var user models.UsersModel
+	err := ctx.Bind(&user)
+	if err != nil {
+		ctx.JSON(500, gin.H{
+			"message": "bad request",
+			"error":   err,
+		})
+		return
+	}
+	res, err := h.userSerivce.GetUserService(user)
+	if err != nil {
+		ctx.JSON(500, gin.H{
+			"message": "server error",
+			"error":   err,
+		})
+		return
+	}
+
+	ctx.JSON(200, res)
 }
 
 func (h *UsersHandler) AddUserHandler(ctx *gin.Context) {
-	user := models.UsersModel{}
-	user.Email = ""
-	_ = h.userSerivce.AddUserService(user)
+	var user models.UsersModel
+	err := ctx.Bind(&user)
+	if err != nil {
+		ctx.JSON(500, gin.H{
+			"message": "bad request",
+			"error":   err,
+		})
+		return
+	}
+	err = h.userSerivce.AddUserService(user)
+	if err != nil {
+		ctx.JSON(500, gin.H{
+			"message": "server error",
+			"error":   err,
+		})
+		return
+	}
+	ctx.JSON(200, gin.H{
+		"message": "user added!",
+	})
 }
 
 func (h *UsersHandler) DeleteUserHandler(ctx *gin.Context) {
 	user := models.UsersModel{}
-	user.Email = ""
-	_ = h.userSerivce.DeleteUserService(user)
+	err := ctx.Bind(&user)
+	if err != nil {
+		ctx.JSON(500, gin.H{
+			"message": "bad request",
+			"error":   err,
+		})
+		return
+	}
+	err = h.userSerivce.DeleteUserService(user)
+	if err != nil {
+		ctx.JSON(500, gin.H{
+			"message": "server error",
+			"error":   err,
+		})
+		return
+	}
+	ctx.JSON(200, gin.H{
+		"message": "user deleted!",
+	})
 }
