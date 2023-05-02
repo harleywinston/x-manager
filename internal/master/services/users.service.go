603169cbf3f839a8002fd47454bab24da93d6b6f
harley winston <harleywinston1993@proton.me>
2023-05-02 20:59:51 +0330

    [ ADD ] add user service and generate passwd

--- internal/master/services/users.service.go
+++ internal/master/services/users.service.go
@@ -1,6 +1,9 @@
 package services
 
 import (
+	"math/rand"
+	"strings"
+
 	"github.com/harleywinston/x-manager/internal/master/database"
 	"github.com/harleywinston/x-manager/internal/master/models"
 )
@@ -14,7 +17,45 @@ func (s *UsersService) GetUserService(user models.UsersModel) (models.UsersModel
 	return res, err
 }
 
+func generateUserPasswd() string {
+	var password strings.Builder
+	lowerCharSet := "abcdedfghijklmnopqrst"
+	upperCharSet := "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	specialCharSet := "!@#$%&*"
+	numberSet := "0123456789"
+	allCharSet := lowerCharSet + upperCharSet + specialCharSet + numberSet
+
+	// Set special character
+	for i := 0; i < 3; i++ {
+		random := rand.Intn(len(specialCharSet))
+		password.WriteString(string(specialCharSet[random]))
+	}
+
+	// Set numeric
+	for i := 0; i < 4; i++ {
+		random := rand.Intn(len(numberSet))
+		password.WriteString(string(numberSet[random]))
+	}
+
+	// Set uppercase
+	for i := 0; i < 2; i++ {
+		random := rand.Intn(len(upperCharSet))
+		password.WriteString(string(upperCharSet[random]))
+	}
+
+	for i := 0; i < 3; i++ {
+		random := rand.Intn(len(allCharSet))
+		password.WriteString(string(allCharSet[random]))
+	}
+	inRune := []rune(password.String())
+	rand.Shuffle(len(inRune), func(i, j int) {
+		inRune[i], inRune[j] = inRune[j], inRune[i]
+	})
+	return string(inRune)
+}
+
 func (s *UsersService) AddUserService(user models.UsersModel) error {
+	user.Passwd = generateUserPasswd()
 	err := s.usersDB.AddUserToDB(user)
 	return err
 }
