eea61d3cc60372f2eef8b824e5c530520d4d17c0
harley winston <harleywinston1993@proton.me>
2023-05-03 21:58:35 +0330

    [ ADD ] groups service and database

--- internal/master/database/groups.database.go
+++ internal/master/database/groups.database.go
@@ -1,1 +1,21 @@
 package database
+
+import (
+	"github.com/harleywinston/x-manager/internal/master/models"
+)
+
+type GroupsDB struct{}
+
+func (db *GroupsDB) AddGroupToDB(group models.Groups) error {
+	return DB.Create(&group).Error
+}
+
+func (db *GroupsDB) GetGroupFromDB(group models.Groups) (models.Groups, error) {
+	var res models.Groups
+	err := DB.First(&res, group).Error
+	return res, err
+}
+
+func (db *GroupsDB) DeleteGroupFromDB(group models.Groups) error {
+	return DB.Delete(&group, group).Error
+}
--- internal/master/services/groups.service.go
+++ internal/master/services/groups.service.go
@@ -1,1 +1,51 @@
 package services
+
+import (
+	"github.com/harleywinston/x-manager/internal/master/database"
+	"github.com/harleywinston/x-manager/internal/master/models"
+)
+
+type GroupsService struct {
+	groupsDB database.GroupsDB
+}
+
+func checkGroupMode(mode string) error {
+	return nil
+}
+
+func checkResourceID(id int) error {
+	return nil
+}
+
+func (s *GroupsService) AddGroupsService(group models.Groups) error {
+	if err := checkGroupMode(group.Mode); err != nil {
+		return err
+	}
+	if err := checkResourceID(group.ResourcesID); err != nil {
+		return err
+	}
+
+	return s.groupsDB.AddGroupToDB(group)
+}
+
+func (s *GroupsService) GetGroupsService(group models.Groups) (models.Groups, error) {
+	if err := checkGroupMode(group.Mode); err != nil {
+		return models.Groups{}, err
+	}
+	if err := checkResourceID(group.ResourcesID); err != nil {
+		return models.Groups{}, err
+	}
+
+	return s.groupsDB.GetGroupFromDB(group)
+}
+
+func (s *GroupsService) DeleteGroupsService(group models.Groups) error {
+	if err := checkGroupMode(group.Mode); err != nil {
+		return err
+	}
+	if err := checkResourceID(group.ResourcesID); err != nil {
+		return err
+	}
+
+	return s.groupsDB.DeleteGroupFromDB(group)
+}
