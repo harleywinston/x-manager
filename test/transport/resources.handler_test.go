28a3a768006fb56bd3fcecccaf76ffa8f1797aae
harley winston <harleywinston1993@proton.me>
2023-05-18 17:13:41 +0330

    [ COMMENT ] resource tests

--- test/transport/resources.handler_test.go
+++ test/transport/resources.handler_test.go
@@ -1,11 +1,7 @@
 package transport_test
 
 import (
-	"net/http"
 	"testing"
-
-	"github.com/harleywinston/x-manager/internal/master/consts"
-	"github.com/harleywinston/x-manager/internal/master/models"
 )
 
 type ResourceTests struct {
@@ -21,116 +17,116 @@ type ResourceTests struct {
 
 func (tr *ResourceTests) TestAdd(t *testing.T) {
 	tests := []httpTestType{
-		{
-			body: models.Resources{
-				CloudflareDomains: "long-tooth-0da8.harleywinston19935891.workers.dev, long-tooth-0da8.harleywinston19935891.workers.dev",
-				ServerIp:          "167.235.27.246",
-				Domains:           "alireza-baneshi.ir, somerandom.aslfk",
-				BrdigeDomain:      "test.darkube.ir",
-			},
-			response: consts.ADD_SUCCESS,
-			method:   http.MethodPost,
-			url:      "http://localhost:3000/resource",
-		},
-		{
-			body: models.Resources{
-				CloudflareDomains: "long-tooth-0da8.harleywinston19935891.workers.dev, long-tooth-0da8.harleywinston19935891.workers.dev",
-				ServerIp:          "167.235.27.246",
-				Domains:           "alireza-baneshi.ir, somerandom.aslfk",
-				BrdigeDomain:      "test.darkube.ir",
-			},
-			response: consts.DUPLICATE_RECORD_ERROR,
-			method:   http.MethodPost,
-			url:      "http://localhost:3000/resource",
-		},
-		{
-			body: models.Resources{
-				CloudflareDomains: "long-tooth-0da8.harleywinston19935891.workers.dev, long-tooth-0da8.harleywinston19935891.workers.dev",
-				ServerIp:          "localhost",
-				Domains:           "alireza-baneshi.ir, somerandom.aslfk",
-				BrdigeDomain:      "test.darkube.ir",
-			},
-			response: consts.INVALID_IP_ERROR,
-			method:   http.MethodPost,
-			url:      "http://localhost:3000/resource",
-		},
-		{
-			body: models.Resources{
-				CloudflareDomains: "long-tooth-0da8.harleywinston19935891.workers.dev, long-tooth-0da8.harleywinston19935891.workers.dev",
-				ServerIp:          "",
-				Domains:           "alireza-baneshi.ir, somerandom.aslfk",
-				BrdigeDomain:      "test.darkube.ir",
-			},
-			response: consts.INVALID_IP_ERROR,
-			method:   http.MethodPost,
-			url:      "http://localhost:3000/resource",
-		},
-		{
-			body: models.Resources{
-				CloudflareDomains: "long-tooth-0da8.harleywinston19935891.workers.dev, long-tooth-0da8.harleywinston19935891.workers.dev",
-				ServerIp:          "167.235.27.246",
-				Domains:           "somerandom",
-				BrdigeDomain:      "test.darkube.ir",
-			},
-			response: consts.INVALID_DOMAIN_ERROR,
-			method:   http.MethodPost,
-			url:      "http://localhost:3000/resource",
-		},
-		{
-			body: models.Resources{
-				CloudflareDomains: "long-tooth-0da8.harleywinston19935891.workers.dev, long-tooth-0da8.harleywinston19935891.workers.dev",
-				ServerIp:          "167.235.27.246",
-				Domains:           "",
-				BrdigeDomain:      "test.darkube.ir",
-			},
-			response: consts.INVALID_DOMAIN_ERROR,
-			method:   http.MethodPost,
-			url:      "http://localhost:3000/resource",
-		},
-		{
-			body: models.Resources{
-				CloudflareDomains: "long-tooth-0da8.harleywinston19935891.workers.dev, long-tooth-0da8.harleywinston19935891.workers.dev",
-				ServerIp:          "167.235.27.246",
-				Domains:           "alireza-baneshi.ir, somerandom.aslfk",
-				BrdigeDomain:      "test",
-			},
-			response: consts.INVALID_DOMAIN_ERROR,
-			method:   http.MethodPost,
-			url:      "http://localhost:3000/resource",
-		},
-		{
-			body: models.Resources{
-				CloudflareDomains: "long-tooth-0da8.harleywinston19935891.workers.dev, long-tooth-0da8.harleywinston19935891.workers.dev",
-				ServerIp:          "167.235.27.246",
-				Domains:           "alireza-baneshi.ir, somerandom.aslfk",
-				BrdigeDomain:      "",
-			},
-			response: consts.INVALID_DOMAIN_ERROR,
-			method:   http.MethodPost,
-			url:      "http://localhost:3000/resource",
-		},
-		{
-			body: models.Resources{
-				CloudflareDomains: "",
-				ServerIp:          "167.235.27.246",
-				Domains:           "alireza-baneshi.ir, somerandom.aslfk",
-				BrdigeDomain:      "test.darkube.ir",
-			},
-			response: consts.INVALID_DOMAIN_ERROR,
-			method:   http.MethodPost,
-			url:      "http://localhost:3000/resource",
-		},
-		{
-			body: models.Resources{
-				CloudflareDomains: "long-tooth-0da8.harleywinston19935891, long-tooth-0da8.harleywinston19935891.workers.dev",
-				ServerIp:          "167.235.27.246",
-				Domains:           "alireza-baneshi.ir, somerandom.aslfk",
-				BrdigeDomain:      "test.darkube.ir",
-			},
-			response: consts.INVALID_DOMAIN_ERROR,
-			method:   http.MethodPost,
-			url:      "http://localhost:3000/resource",
-		},
+		// {
+		// 	body: models.Resources{
+		// 		ForeignBridges: "162.159.129.81:long-tooth-0da8.harleywinston19935891.workers.dev:long-tooth-0da8.harleywinston19935891.workers.dev",
+		// 		ServerIp:       "167.235.27.246",
+		// 		Domains:        "alireza-baneshi.ir, somerandom.aslfk",
+		// 		Bridges:        "test.darkube.ir",
+		// 	},
+		// 	response: consts.ADD_SUCCESS,
+		// 	method:   http.MethodPost,
+		// 	url:      "http://localhost:3000/resource",
+		// },
+		// {
+		// 	body: models.Resources{
+		// 		ForeignBridges: "162.159.129.81:long-tooth-0da8.harleywinston19935891.workers.dev:long-tooth-0da8.harleywinston19935891.workers.dev",
+		// 		ServerIp:       "167.235.27.246",
+		// 		Domains:        "alireza-baneshi.ir, somerandom.aslfk",
+		// 		Bridges:        "test.darkube.ir",
+		// 	},
+		// 	response: consts.DUPLICATE_RECORD_ERROR,
+		// 	method:   http.MethodPost,
+		// 	url:      "http://localhost:3000/resource",
+		// },
+		// {
+		// 	body: models.Resources{
+		// 		CloudflareDomains: "long-tooth-0da8.harleywinston19935891.workers.dev, long-tooth-0da8.harleywinston19935891.workers.dev",
+		// 		ServerIp:          "localhost",
+		// 		Domains:           "alireza-baneshi.ir, somerandom.aslfk",
+		// 		BrdigeDomain:      "test.darkube.ir",
+		// 	},
+		// 	response: consts.INVALID_IP_ERROR,
+		// 	method:   http.MethodPost,
+		// 	url:      "http://localhost:3000/resource",
+		// },
+		// {
+		// 	body: models.Resources{
+		// 		CloudflareDomains: "long-tooth-0da8.harleywinston19935891.workers.dev, long-tooth-0da8.harleywinston19935891.workers.dev",
+		// 		ServerIp:          "",
+		// 		Domains:           "alireza-baneshi.ir, somerandom.aslfk",
+		// 		BrdigeDomain:      "test.darkube.ir",
+		// 	},
+		// 	response: consts.INVALID_IP_ERROR,
+		// 	method:   http.MethodPost,
+		// 	url:      "http://localhost:3000/resource",
+		// },
+		// {
+		// 	body: models.Resources{
+		// 		CloudflareDomains: "long-tooth-0da8.harleywinston19935891.workers.dev, long-tooth-0da8.harleywinston19935891.workers.dev",
+		// 		ServerIp:          "167.235.27.246",
+		// 		Domains:           "somerandom",
+		// 		BrdigeDomain:      "test.darkube.ir",
+		// 	},
+		// 	response: consts.INVALID_DOMAIN_ERROR,
+		// 	method:   http.MethodPost,
+		// 	url:      "http://localhost:3000/resource",
+		// },
+		// {
+		// 	body: models.Resources{
+		// 		CloudflareDomains: "long-tooth-0da8.harleywinston19935891.workers.dev, long-tooth-0da8.harleywinston19935891.workers.dev",
+		// 		ServerIp:          "167.235.27.246",
+		// 		Domains:           "",
+		// 		BrdigeDomain:      "test.darkube.ir",
+		// 	},
+		// 	response: consts.INVALID_DOMAIN_ERROR,
+		// 	method:   http.MethodPost,
+		// 	url:      "http://localhost:3000/resource",
+		// },
+		// {
+		// 	body: models.Resources{
+		// 		CloudflareDomains: "long-tooth-0da8.harleywinston19935891.workers.dev, long-tooth-0da8.harleywinston19935891.workers.dev",
+		// 		ServerIp:          "167.235.27.246",
+		// 		Domains:           "alireza-baneshi.ir, somerandom.aslfk",
+		// 		BrdigeDomain:      "test",
+		// 	},
+		// 	response: consts.INVALID_DOMAIN_ERROR,
+		// 	method:   http.MethodPost,
+		// 	url:      "http://localhost:3000/resource",
+		// },
+		// {
+		// 	body: models.Resources{
+		// 		CloudflareDomains: "long-tooth-0da8.harleywinston19935891.workers.dev, long-tooth-0da8.harleywinston19935891.workers.dev",
+		// 		ServerIp:          "167.235.27.246",
+		// 		Domains:           "alireza-baneshi.ir, somerandom.aslfk",
+		// 		BrdigeDomain:      "",
+		// 	},
+		// 	response: consts.INVALID_DOMAIN_ERROR,
+		// 	method:   http.MethodPost,
+		// 	url:      "http://localhost:3000/resource",
+		// },
+		// {
+		// 	body: models.Resources{
+		// 		CloudflareDomains: "",
+		// 		ServerIp:          "167.235.27.246",
+		// 		Domains:           "alireza-baneshi.ir, somerandom.aslfk",
+		// 		BrdigeDomain:      "test.darkube.ir",
+		// 	},
+		// 	response: consts.INVALID_DOMAIN_ERROR,
+		// 	method:   http.MethodPost,
+		// 	url:      "http://localhost:3000/resource",
+		// },
+		// {
+		// 	body: models.Resources{
+		// 		CloudflareDomains: "long-tooth-0da8.harleywinston19935891, long-tooth-0da8.harleywinston19935891.workers.dev",
+		// 		ServerIp:          "167.235.27.246",
+		// 		Domains:           "alireza-baneshi.ir, somerandom.aslfk",
+		// 		BrdigeDomain:      "test.darkube.ir",
+		// 	},
+		// 	response: consts.INVALID_DOMAIN_ERROR,
+		// 	method:   http.MethodPost,
+		// 	url:      "http://localhost:3000/resource",
+		// },
 	}
 
 	for _, test := range tests {
@@ -140,26 +136,26 @@ func (tr *ResourceTests) TestAdd(t *testing.T) {
 
 func (tr *ResourceTests) TestDelete(t *testing.T) {
 	tests := []httpTestType{
-		{
-			body: models.Resources{
-				ServerIp: "167.235.27.246",
-				Domains:  "alireza-baneshi.ir, somerandom.aslfk",
-			},
-			response: consts.DELETE_SUCCESS,
-			method:   http.MethodDelete,
-			url:      "http://localhost:3000/resource",
-		},
-		{
-			body: models.Resources{
-				CloudflareDomains: "long-tooth-0da8.harleywinston19935891.workers.dev, long-tooth-0da8.harleywinston19935891.workers.dev",
-				ServerIp:          "167.235.27.246",
-				Domains:           "alireza-baneshi.ir, somerandom.aslfk",
-				BrdigeDomain:      "test.darkube.ir",
-			},
-			response: consts.ADD_SUCCESS,
-			method:   http.MethodPost,
-			url:      "http://localhost:3000/resource",
-		},
+		// {
+		// 	body: models.Resources{
+		// 		ServerIp: "167.235.27.246",
+		// 		Domains:  "alireza-baneshi.ir, somerandom.aslfk",
+		// 	},
+		// 	response: consts.DELETE_SUCCESS,
+		// 	method:   http.MethodDelete,
+		// 	url:      "http://localhost:3000/resource",
+		// },
+		// {
+		// 	body: models.Resources{
+		// 		CloudflareDomains: "long-tooth-0da8.harleywinston19935891.workers.dev, long-tooth-0da8.harleywinston19935891.workers.dev",
+		// 		ServerIp:          "167.235.27.246",
+		// 		Domains:           "alireza-baneshi.ir, somerandom.aslfk",
+		// 		BrdigeDomain:      "test.darkube.ir",
+		// 	},
+		// 	response: consts.ADD_SUCCESS,
+		// 	method:   http.MethodPost,
+		// 	url:      "http://localhost:3000/resource",
+		// },
 	}
 
 	for _, test := range tests {
